9c28ee3a7ad5fd0ae5594779e76f0e565f955108
Maksim Shuklin <makshuk@my.com>
2023-07-10 09:47:19 +0300

    test 7 v1

--- src/index.ts
+++ src/index.ts
@@ -1,98 +1,62 @@
-interface IClasses {
-	name: string;
-	startsAt: string;
-	duration: number;
+enum TransferStatus {
+	Pending = 'pending',
+	Rejected = 'rejected',
+	Completed = 'completed',
 }
 
-interface IWillStartsAt extends Omit<IClasses, 'startsAt'> {
-	willStartsAt: string;
+enum ErrorMessages {
+	NotFound = 'Not found: 404',
+	NotEnoughSpace = 'Not enough space: 507',
+	Forbidden = 'Forbidden: 403',
 }
 
-interface ICurrClients extends Pick<IClasses, 'name'> {
-	age: number | string;
-	gender: string;
-	timeLeft: string;
+interface ITransfer {
+	path: string;
+	data: string[];
+	date?: Date;
+	start: (p: string, d: string[]) => string;
+	stop: (reason: string) => string;
 }
 
-interface IExClients extends Omit<ICurrClients, 'timeLeft'> {
-	makeCallFor: Date;
+interface TransferError {
+	message: ErrorMessages;
 }
 
-type IFutureClients = Pick<IExClients, 'name' | 'makeCallFor'>;
+// Класс должен имплементировать ITransfer и TransferError
+class SingleFileTransfer implements ITransfer, TransferError {
+	message: ErrorMessages;
+	path: string;
+	data: string[];
+	date?: Date | undefined;
+	status: TransferStatus;
+	constructor() {
+		this.status = TransferStatus.Pending;
+	}
 
-interface IFitnessClubCenter {
-	clubName: string;
-	location: string;
-	classes: IClasses[];
-	futureClasses: IWillStartsAt[];
-	currClients: ICurrClients[];
-	exClients: IExClients[];
-	futureClients: IFutureClients[];
+	start(p: string, d: string[]): string {
+		this.status = TransferStatus.Pending;
+		return `p: ${p} d: ${d.join(' ')}`;
+	}
+	stop(reason: string): string {
+		this.status = TransferStatus.Completed;
+		return `${new Date()}: Transfer: ${this.status}, reason: ${reason};`;
+	}
+
+	checkTransferStatus(): string {
+		return this.status;
+	}
+
+	getError(): string {
+		return `Transfer status:${this.status}, error ${ErrorMessages.Forbidden}`;
+	}
+
+	// Необходимо создать метод, который будет останавливать передачу данных
+	// И возвращать строку с причиной и датой остановки (Дата в любом формате)
+	// Необходимо создать метод, который будет возвращать строку, содержащую
+	// Статус передачи и любое сообщение об ошибке. На ваш выбор или отталкиваться от приходящего аргумента
+	// Метод может показаться странным, но может использоваться для тестов, например
 }
-const fitnessClubCenter: IFitnessClubCenter = {
-	clubName: 'Fitness club Center',
-	location: 'central ave. 45, 5th floor',
-	classes: [
-		{
-			name: 'yoga',
-			startsAt: '8:00 AM',
-			duration: 60,
-		},
-		{
-			name: 'trx',
-			startsAt: '11:00 AM',
-			duration: 45,
-		},
-		{
-			name: 'swimming',
-			startsAt: '3:00 PM',
-			duration: 70,
-		},
-	],
-	futureClasses: [
-		{
-			name: 'boxing',
-			willStartsAt: '6:00 PM',
-			duration: 40,
-		},
-		{
-			name: 'breath training',
-			willStartsAt: '8:00 PM',
-			duration: 30,
-		},
-	],
-	currClients: [
-		{
-			name: 'John Smith',
-			age: '-',
-			gender: 'male',
-			timeLeft: '1 month',
-		},
-		{
-			name: 'Alise Smith',
-			age: 35,
-			gender: 'female',
-			timeLeft: '3 month',
-		},
-		{
-			name: 'Ann Sonne',
-			age: 24,
-			gender: 'female',
-			timeLeft: '5 month',
-		},
-	],
-	exClients: [
-		{
-			name: 'Tom Smooth',
-			age: 50,
-			gender: 'male',
-			makeCallFor: new Date('2023-08-12'),
-		},
-	],
-	futureClients: [
-		{
-			name: 'Maria',
-			makeCallFor: new Date('2023-07-10'),
-		},
-	],
-};
+
+const res = new SingleFileTransfer();
+console.log(res.checkTransferStatus());
+console.log(res.stop('not data'));
